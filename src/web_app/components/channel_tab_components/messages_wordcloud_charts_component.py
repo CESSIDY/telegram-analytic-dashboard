a224f1ce8423875a2d14f79c6ae6adc0af309c97
Vadym <vrupa1713@gmail.com>
2024-03-19 10:03:19 +0100

    - feat(wordcloud): date picker logic and component;

--- src/web_app/components/channel_tab_components/messages_wordcloud_charts_component.py
+++ src/web_app/components/channel_tab_components/messages_wordcloud_charts_component.py
@@ -1,6 +1,7 @@
 import logging
 from io import BytesIO
 import base64
+from datetime import date
 
 from dash import html, dcc, callback, Output, Input, State
 import dash_bootstrap_components as dbc
@@ -8,6 +9,7 @@ from pandas import DataFrame
 import nltk
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
+import numpy as np
 
 from web_app.components.utils.basic_components import generate_section_banner
 from .base_dashboard_component import BaseDashboardComponent
@@ -22,9 +24,15 @@ class MessageWordCloudChartsComponent(BaseDashboardComponent):
     DEFAULT_STOPWORDS_LANGUAGES = ['english', 'ukrainian']
     LANGUAGES_OPTIONS = stopwords_custom_languages + stopwords.fileids()
 
+    def __init__(self):
+        super().__init__()
+        self.first_message_date, self.last_message_date = None, None
+
     def set_messages_df(self, messages_df: DataFrame):
         super().set_messages_df(messages_df)
-        self.messages_df = self.messages_df[:-self.MOST_RECENT_MESSAGE_OFFSET]
+        self.messages_df: DataFrame = self.messages_df[:-self.MOST_RECENT_MESSAGE_OFFSET]
+        all_dates = self.messages_df['date'].sort_values(ascending=True).values
+        self.first_message_date, self.last_message_date = all_dates[0], all_dates[-1]
 
     def build(self):
         return html.Div(
@@ -40,12 +48,13 @@ class MessageWordCloudChartsComponent(BaseDashboardComponent):
         callback(
             Output('image_messages_wc', 'src'),
             [Input('build-messages-wc', 'n_clicks')],
-            [State('wc-languages-dropdown', 'value')]
+            [State('wc-languages-dropdown', 'value'),
+             State('wc-date-picker-range', 'start_date'),
+             State('wc-date-picker-range', 'end_date')]
             # background=True
         )(self.build_wc_image)
 
     def build_interacted_components(self):
-        # TODO: Add date pick range !!!!!!!!!!!!!!!!
         return html.Div(
             [
                 dcc.Loading(
@@ -66,12 +75,21 @@ class MessageWordCloudChartsComponent(BaseDashboardComponent):
                     multi=True,
                     className='wc-languages-dropdown',
                     placeholder='Select stopwords languages'
+                ),
+                dcc.DatePickerRange(
+                    id='wc-date-picker-range',
+                    min_date_allowed=self.first_message_date,
+                    max_date_allowed=self.last_message_date,
+                    initial_visible_month=self.first_message_date,
+                    start_date=self.first_message_date,
+                    end_date=self.last_message_date,
+                    className='wc-date-picker'
                 )
             ],
             className='wc_interacted_container',
         )
 
-    def build_wc_image(self, n_clicks, needed_languages):
+    def build_wc_image(self, n_clicks, needed_languages, start_date, end_date):
         img = BytesIO()
 
         all_stopwords = []
@@ -83,7 +101,17 @@ class MessageWordCloudChartsComponent(BaseDashboardComponent):
             else:
                 all_stopwords.extend(custom_stopwords(lang))
 
-        messages_df = self.messages_df.copy()
+        if not start_date:
+            start_date = self.first_message_date
+        else:
+            start_date = date.fromisoformat(start_date)
+        if not end_date:
+            end_date = self.last_message_date
+        else:
+            end_date = date.fromisoformat(end_date)
+
+        messages_df = self.messages_df[(self.messages_df['date'] >= start_date) &
+                                       (self.messages_df['date'] <= end_date)].copy()
         messages_df['message_nostop'] = messages_df['message'].apply(lambda x: clean_text_from_stopwords(x.lower(),
                                                                                                          all_stopwords))
         full_text_from_messages = "".join(messages_df['message_nostop'].tolist())
